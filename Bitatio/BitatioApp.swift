9d3146585057a5656e29fcfea0890939a9b1c199
Sebastien Green <45818412+sebygreen@users.noreply.github.com>
2023-03-27 20:02:27 +0200

    Initial Commit

---
+++ Bitatio/BitatioApp.swift
@@ -0,0 +1,17 @@
+//
+//  BitatioApp.swift
+//  Bitatio
+//
+//  Created by Sebastien Green on 27/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct BitatioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
